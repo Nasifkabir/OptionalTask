fbcbcffc3909fe6aed4e9cd58175a07e2237244d
Nasifkabir <nasifjubian37@gmail.comm>
2024-09-09 16:38:32 +0600

    File added

---
+++ Task.java
@@ -0,0 +1,15 @@
+/* Java and C# section */
+public class Task {
+    public static void main(String[] args) {
+        System.out.println("Itransition"); // Java output
+    }
+
+    public static void Main(string[] args) {
+        System.Console.WriteLine("Itransition"); // C# output
+    }
+}
+
+/* JavaScript section */ 
+if (typeof console !== 'undefined') {
+    console.log("Itransition"); // JavaScript output
+}
